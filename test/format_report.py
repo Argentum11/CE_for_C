72c898617efe83a916c323b2d5f3a55580bbcd11
Leo Predator <leochang658@gmail.com>
2023-04-13 08:24:04 +0800

    TEST: classify result and test time in report.md

--- test/format_report.py
+++ test/format_report.py
@@ -8,10 +8,10 @@ def escape_ansi(line):
 markdown_with_ansi = ""
 with open("report.md",'r') as my_file:
       markdown_with_ansi = my_file.read()
-md_without_ansi = escape_ansi(line = markdown_with_ansi)
+md_without_ansi = "# Result\n\n" + escape_ansi(line = markdown_with_ansi)
 tz = timezone(timedelta(hours=+8))
 now = datetime.now(tz)
-date_and_time = "Report generated on " + str(now.strftime("%Y-%m-%d at %H:%M:%S")) + "\n"
+date_and_time = "\n### Test time\n\nReport generated on " + str(now.strftime("%Y-%m-%d at %H:%M:%S")) + "\n"
 md_plus_time = md_without_ansi + date_and_time
 with open("report.md",'w') as my_file:
       my_file.write(md_plus_time)
--- test/report.md
+++ test/report.md
@@ -1,7 +1,12 @@
+# Result
+
 |           filepath            | passed | SUBTOTAL |
 | ----------------------------- | -----: | -------: |
 | test_arithmetic_operations.py |      5 |        5 |
 | test_math_function.py         |      2 |        2 |
 | test_var.py                   |      1 |        1 |
 | TOTAL                         |      8 |        8 |
-Report generated on 2023-04-02 at 21:36:22
+
+### Test time
+
+Report generated on 2023-04-13 at 08:21:25
