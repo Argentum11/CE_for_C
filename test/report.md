dcf4f6234a9a9e88ec9a83864ba1556aeb576f93
Leo Spin <leochang658@gmail.com>
2023-06-04 23:39:08 +0800

    TEST: add sin cos tan test

--- test/report.md
+++ test/report.md
@@ -4,9 +4,9 @@
 | ----------------------------- | -----: | -------: |
 | test_arithmetic_operations.py |      5 |        5 |
 | test_comparison.py            |      5 |        5 |
-| test_math_function.py         |      1 |        1 |
-| TOTAL                         |     11 |       11 |
+| test_math_function.py         |      4 |        4 |
+| TOTAL                         |     14 |       14 |
 
 ### Test time
 
-Report generated on 2023-06-04 at 22:35:03
+Report generated on 2023-06-04 at 23:37:57
--- test/test_math_function.py
+++ test/test_math_function.py
@@ -22,3 +22,66 @@ def test_abs():
     # negative
     negative_integer = get_random_number(NEGATIVE, INT)
     run_abs(negative_integer)
+#######################################################################
+def degree_to_radian(degree):
+    PI = 3.14159265359
+    return f'{degree}*{PI}/180'
+
+SIN = "sin"
+COS = "cos"
+TAN = "tan"
+
+def trigonometric_command(function, radian):
+    return f'cout<<{function}({radian})<<endl;'
+
+sin_dict = {
+    30: 0.5,
+    60: 0.866025,
+    90: 1
+}
+
+def run_sin(degree):
+    radian = degree_to_radian(degree)
+    command = trigonometric_command(SIN, radian)
+    expected_output = sin_dict[degree]
+    case = Case(command, expected_output)
+    assert case.expected_output == run_command(case)
+
+def test_sin():
+    for degree in sin_dict.keys():
+        run_sin(degree)
+#######################################################################
+cos_dict = {
+    30: 0.866025,
+    60: 0.5,
+    180: -1
+}
+
+def run_cos(degree):
+    radian = degree_to_radian(degree)
+    command = trigonometric_command(COS, radian)
+    expected_output = cos_dict[degree]
+    case = Case(command, expected_output)
+    assert case.expected_output == run_command(case)
+
+def test_cos():
+    for degree in cos_dict.keys():
+        run_cos(degree)
+#######################################################################
+tan_dict = {
+    30: 0.57735,
+    45: 1,
+    60: 1.73205,
+    
+}
+
+def run_tan(degree):
+    radian = degree_to_radian(degree)
+    command = trigonometric_command(TAN, radian)
+    expected_output = tan_dict[degree]
+    case = Case(command, expected_output)
+    assert case.expected_output == run_command(case)
+
+def test_tan():
+    for degree in tan_dict.keys():
+        run_tan(degree)
